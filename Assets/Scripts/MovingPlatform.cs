9a8dfb4aab7aaca0786fa7f433b210f53efca3bb
Nguyễn Hoàng Đăng Khoa <nguyenkhoa26052006@gmail.com>
2025-06-27 20:49:21 +0700

    chỉnh sửa xíu movingplatform

--- Assets/Scripts/MovingPlatform.cs
+++ Assets/Scripts/MovingPlatform.cs
@@ -1,52 +1,49 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class MovingPlatform : MonoBehaviour
 {
-    public float tocDoDiChuyen = 1f;
-    public Transform start;
-    public Transform end;
+    [SerializeField] private Transform start;
+    [SerializeField] private Transform end;
+    [SerializeField] private float speed = 3f;
 
-    private Vector2 diemMucTieu;
-    private Rigidbody2D rb;
-    private Vector2 previousPosition;
+    private Vector3 target;
 
-    private void Start()
+    void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
-        rb.bodyType = RigidbodyType2D.Kinematic;
-        diemMucTieu = end.position;
-        previousPosition = rb.position;
+        target = start.position;
     }
 
-    private void FixedUpdate()
+    void Update()
     {
-        if (Vector2.Distance(rb.position, diemMucTieu) < 0.1f)
-        {
-            diemMucTieu = diemMucTieu == (Vector2)start.position ? end.position : start.position;
-        }
+        transform.position = Vector3.MoveTowards(transform.position, target, speed * Time.deltaTime);
 
-        Vector2 newPosition = Vector2.MoveTowards(rb.position, diemMucTieu, tocDoDiChuyen * Time.fixedDeltaTime);
-        rb.MovePosition(newPosition);
-    }
-
-    private void OnCollisionStay2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("Player"))
+        if (Vector3.Distance(transform.position, target) < 0.1f)
         {
-            Rigidbody2D playerRb = collision.collider.attachedRigidbody;
-            if (playerRb != null)
+            if (target == start.position)
             {
-                Vector2 delta = rb.position - previousPosition;
-                playerRb.position += delta;
+                target = end.position;
+            }
+            else
+            {
+                target = start.position;
             }
         }
     }
 
-    private void LateUpdate()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        previousPosition = rb.position;
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
     }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+
 }
